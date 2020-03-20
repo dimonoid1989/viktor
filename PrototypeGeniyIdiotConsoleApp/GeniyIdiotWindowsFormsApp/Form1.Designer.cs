58ef7115b6521f25b61a362b4d757c8b1e54e994
Victor Byliev <ya.vic4592@yandex.ru>
2020-03-20 17:59:54 +0300

    Добавили элементы управления

--- PrototypeGeniyIdiotConsoleApp/GeniyIdiotWindowsFormsApp/Form1.Designer.cs
+++ PrototypeGeniyIdiotConsoleApp/GeniyIdiotWindowsFormsApp/Form1.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.questionTextLabel = new System.Windows.Forms.Label();
+            this.questionAnswerTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // questionTextLabel
+            // 
+            this.questionTextLabel.AutoSize = true;
+            this.questionTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.questionTextLabel.Location = new System.Drawing.Point(42, 40);
+            this.questionTextLabel.Name = "questionTextLabel";
+            this.questionTextLabel.Size = new System.Drawing.Size(131, 20);
+            this.questionTextLabel.TabIndex = 0;
+            this.questionTextLabel.Text = "Текст вопроса";
+            // 
+            // questionAnswerTextBox
+            // 
+            this.questionAnswerTextBox.Location = new System.Drawing.Point(47, 94);
+            this.questionAnswerTextBox.Name = "questionAnswerTextBox";
+            this.questionAnswerTextBox.Size = new System.Drawing.Size(126, 20);
+            this.questionAnswerTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Далее";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(448, 298);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.questionAnswerTextBox);
+            this.Controls.Add(this.questionTextLabel);
             this.Name = "Form1";
             this.Text = "Гений-Идиот";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label questionTextLabel;
+        private System.Windows.Forms.TextBox questionAnswerTextBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
