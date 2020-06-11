64eb060726ca86c08a2a7dbdeeb997f3569d9e18
Victor Byliev <ya.vic4592@yandex.ru>
2020-06-11 16:34:48 +0300

    Создание нового проекта, пустой формы и пустого класса

---
+++ NewPractiseBallsWindowsFormsApp/Fruit_Ninja_ballApp/FruitNinjaForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fruit_Ninja_ballApp
+{
+    public partial class FruitNinjaForm : Form
+    {
+        public FruitNinjaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
